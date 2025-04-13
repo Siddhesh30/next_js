0ec0f1bfc674eae97916e984942d696215ac39c1
Siddhesh30 <sidspalimkar07@gmail.com>
2025-04-13 19:19:46 +0530

    Assets and Css changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl text-black font-black">
+  <h1 className="text-3xl text-black font-black font-inter">
     Welcome to the world of Next.js
   </h1>
 );
